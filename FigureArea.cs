ba51de6073014b919f4e0b107f756ea69bc245e9
Zimovsky <sergey@zimovsky.pro>
2020-05-12 06:41:55 +0300

    Initial commit.

---
+++ FigureArea.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class FigureArea : MonoBehaviour
+{
+    
+    public static double UnknownFigureArea(double radius) {
+        Debug.Log("It's a circle!");
+        double area = CircleAreaByRadius(radius);
+        return area;
+    }
+
+    public static double UnknownFigureArea(double sideA, double sideB) {
+        Debug.Log("It's a rectangle!");
+        double area = RectangleAreaBy2Sides(sideA, sideB);
+        return area;
+    }
+
+    public static double UnknownFigureArea(double sideA, double sideB, double sideC) {
+        Debug.Log("It's a triangle!");
+        double area = TriangleAreaBy3Sides(sideA, sideB, sideC);
+        return area;
+    }
+
+    /// <summary>
+    /// Calculates total area of a circle by its radius.
+    /// </summary>
+    /// <param name="radius">Radius of the circle.</param>
+    /// <returns>Total area of the circle.</returns>
+    public static double CircleAreaByRadius(double radius) {
+        double area;
+        area = Math.PI * Math.Pow(radius, 2);
+        Debug.Log("Math.PI * Math.Pow(" + radius + ", 2) = " + area);
+        return area;
+    }
+
+    /// <summary>
+    /// Calculates total area of a rectangle by its 2 sides.
+    /// </summary>
+    /// <param name="sideA">Length of the side A.</param>
+    /// <param name="sideB">Length of the side B.</param>
+    /// <returns>Total area of the rectangle.</returns>
+    public static double RectangleAreaBy2Sides(double sideA, double sideB) {
+        double area;
+        area = sideA * sideB;
+        Debug.Log(sideA + " * " + sideB + " = " + area);
+        return area;
+    }
+
+    /// <summary>
+    /// Calculates total area of a triangle by its 3 sides.
+    /// </summary>
+    /// <param name="sideA">Length of the side A.</param>
+    /// <param name="sideB">Length of the side B.</param>
+    /// <param name="sideC">Length of the side C.</param>
+    /// <returns></returns>
+    public static double TriangleAreaBy3Sides(double sideA, double sideB, double sideC) {
+        double area;
+
+        double p = (sideA + sideB + sideC) / 2;
+        area = Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        Debug.Log("Math.Sqrt(" + p + " * (" + p + " - " + sideA + ") * (" + p + " - " + sideB + ") * (" + p + " - " + sideB + ")) = " + area);
+        return area;
+    }
+
+    /// <summary>
+    /// Checks if the triangle is a right one.
+    /// </summary>
+    /// <param name="sideA">Length of the side A.</param>
+    /// <param name="sideB">Length of the side B.</param>
+    /// <param name="sideC">Length of the side C.</param>
+    /// <returns>Rerurns true if it is right, false - if not.</returns>
+    public static bool IsRightTriangle(double sideA, double sideB, double sideC) {
+        if (Math.Pow(sideA, 2) + Math.Pow(sideB, 2) == Math.Pow(sideC, 2) || Math.Pow(sideB, 2) + Math.Pow(sideC, 2) == Math.Pow(sideA, 2) || Math.Pow(sideA, 2) + Math.Pow(sideC, 2) == Math.Pow(sideB, 2)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField]
+    GameObject inputParamA;
+    [SerializeField]
+    GameObject inputParamB;
+    [SerializeField]
+    GameObject inputParamC;
+
+    [SerializeField]
+    Text textParamA;
+    [SerializeField]
+    Text textParamB;
+    [SerializeField]
+    Text textParamC;
+
+    [SerializeField]
+    Text labelFigure;
+    [SerializeField]
+    Text labelResult;
+
+    double paramA;
+    double paramB;
+    double paramC;
+    double totalArea;
+
+    public string ParamA {
+        get => paramA.ToString();
+        set {
+            double toDouble;
+            if (double.TryParse(value, out toDouble) == true) {
+                paramA = toDouble;
+            }
+        }
+    }
+
+    public string ParamB {
+        get => paramB.ToString();
+        set {
+            double toDouble;
+            if (double.TryParse(value, out toDouble) == true) {
+                paramB = toDouble;
+            }
+        }
+    }
+
+    public string ParamC {
+        get => paramC.ToString();
+        set {
+            double toDouble;
+            if (double.TryParse(value, out toDouble) == true) {
+                paramC = toDouble;
+            }
+        }
+    }
+
+    public string TotalArea {
+        get => totalArea.ToString();
+        set {
+            double toDouble;
+            if (double.TryParse(value, out toDouble) == true) {
+                totalArea = toDouble;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInputFields();
+    }
+
+    public void ButtonCalculate() {
+        Debug.Log(paramA + ", " + paramB + ", " + paramC);
+        if (paramA != 0 && paramB == 0 && paramC == 0) {
+            labelResult.text = "Total area: " + FigureArea.CircleAreaByRadius(paramA).ToString();
+        } else if (paramA != 0 && paramB != 0 && paramC == 0) {
+            labelResult.text = "Total area: " + FigureArea.RectangleAreaBy2Sides(paramA, paramB).ToString();
+        } else if (paramA != 0 && paramB != 0 && paramC != 0) {
+            labelResult.text = "Total area: " + FigureArea.TriangleAreaBy3Sides(paramA, paramB, paramC).ToString();
+        }
+    }
+
+    void CheckInputFields() {
+        if (textParamA.text != "") {
+            inputParamB.SetActive(true);
+            labelFigure.text = "It is a circle! :D";
+            if (textParamB.text != "") {
+                inputParamC.SetActive(true);
+                labelFigure.text = "It is a rectangle! :D";
+                if (textParamC.text != "") {
+                    if (FigureArea.IsRightTriangle(paramA, paramB, paramC) == true) {
+                        labelFigure.text = "It is a right triangle! :D";
+                    } else {
+                        labelFigure.text = "It is a triangle! :D";
+                    }
+                }
+            }
+            else {
+                inputParamC.SetActive(false);
+                textParamC.text = "";
+            }
+        }
+        else {
+            inputParamB.SetActive(false);
+            textParamB.text = "";
+            labelFigure.text = "What figure is it?";
+        }
+        
+    }
+
+
+}
